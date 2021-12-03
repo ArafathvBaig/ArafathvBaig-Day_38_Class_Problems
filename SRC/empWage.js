9be9a72f36896625d2cdf1bd7c9031d182452d46
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-12-04 00:19:29 +0530

    Updated: Files With UC4

--- SRC/empWage.js
+++ SRC/empWage.js
@@ -1,10 +1,11 @@
-//UC-3
+//UC-4
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const IS_ABSENT = 0;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 function getWorkingHours(empCheck) 
 {
@@ -19,8 +20,10 @@ function getWorkingHours(empCheck)
     }
 }
 let empHrs =0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-console.log("Emp Check: " + empCheck);
-empHrs = getWorkingHours(empCheck);
+for (let day=0; day < NUM_OF_WORKING_DAYS; day++)
+{
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);
+}
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Emp Wage: " + empWage);
+console.log("Total Hours: "+empHrs+"Emp Wage: " + empWage);
