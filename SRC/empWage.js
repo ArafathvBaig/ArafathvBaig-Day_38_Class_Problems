266f687f8656730f2a9ddbefd90550e4cff03910
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-12-04 00:04:56 +0530

    Updated: Files With UC3

--- SRC/empWage.js
+++ SRC/empWage.js
@@ -1,4 +1,4 @@
-//UC-2
+//UC-3
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const IS_ABSENT = 0;
@@ -6,21 +6,21 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-console.log("Emp Check: "+empCheck);
-switch(empCheck)
+function getWorkingHours(empCheck) 
 {
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs = 0;
-        break;
+    switch (empCheck) 
+    {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
-
+let empHrs =0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+console.log("Emp Check: " + empCheck);
+empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Emp Wage: "+empWage);
+console.log("Emp Wage: " + empWage);
